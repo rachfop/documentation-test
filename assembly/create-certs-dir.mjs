cbfe0e9ed5820ce403359fbe0ed87d6444dfd08b
rachfop <prachford@icloud.com>
2023-10-23 13:32:29 -0700

    testing again

--- assembly/create-certs-dir.mjs
+++ assembly/create-certs-dir.mjs
@@ -8,15 +8,11 @@ const targetDir = join(currentDir, "secure");
 await fs.mkdir(targetDir, { recursive: true });
 await fs.writeFile(join(targetDir, "docs-assembly.pem"), process.env.TEMPORAL_CLIENT_CERT);
 await fs.writeFile(join(targetDir, "docs-assembly.key"), process.env.TEMPORAL_CLIENT_KEY);
-await fs.writeFile(
-  join(targetDir, "cloud-connection.json"),
-  JSON.stringify(
-    {
-      "address": "docs-assembly.a2dd6.tmprl.cloud:7233",
-      "namespace": "docs-assembly.a2dd6",
-      "unique_id": "GitHub-Actions-Assembly-Test",
-    },
-    null,
-    2
-  )
-);
+
+const connectionConfig = {
+  address: "docs-assembly.a2dd6.tmprl.cloud:7233",
+  namespace: "docs-assembly.a2dd6",
+  unique_id: "GitHub-Actions-Assembly-Test"
+};
+
+await fs.writeFile(join(targetDir, "cloud-connection.json"), JSON.stringify(connectionConfig, null, 2));
